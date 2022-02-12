d90613b3f5a9d95bda29cfe699002240b3c41e83
pStrachota <p.strachota52@gmail.com>
2022-02-12 22:17:22 +0100

    add MIT license to project

--- src/main/java/SudokuBoard.java
+++ src/main/java/SudokuBoard.java
@@ -1,3 +1,29 @@
+/*
+ * #%L
+ * KOMPO_PROJECT
+ * %%
+ * Copyright (C) 2021 - 2022 TUL
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,12 +43,8 @@ public class SudokuBoard {
         this.backtrackingSudokuSolver.solve(this);
     }
 
-    public SudokuRow getRow(int y) {
-        SudokuRow sudokuRow = new SudokuRow();
-        for (int column = 0; column < sudokuSize; column++) {
-            sudokuRow.setSudokuField(column, this.board.get(y * sudokuSize + column).getFieldValue());
-        }
-        return sudokuRow;
+    public boolean checkBoard() {
+        return isRowCorrect() && isColumnCorrect() && isBoxCorrect();
     }
 
     private boolean isRowCorrect() {
@@ -43,8 +65,24 @@ public class SudokuBoard {
         return true;
     }
 
-    public boolean checkBoard() {
-        return isRowCorrect() && isColumnCorrect() && isBoxCorrect();
+    private boolean isBoxCorrect() {
+        for (int row = 0; row < sudokuSize; row++) {
+            for (int column = 0; column < sudokuSize; column++) {
+                if (!this.getBox(row, column).verify()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public SudokuRow getRow(int y) {
+        SudokuRow sudokuRow = new SudokuRow();
+        for (int column = 0; column < sudokuSize; column++) {
+            sudokuRow.setSudokuField(column,
+                    this.board.get(y * sudokuSize + column).getFieldValue());
+        }
+        return sudokuRow;
     }
 
     public SudokuColumn getColumn(int x) {
@@ -68,17 +106,6 @@ public class SudokuBoard {
             }
         }
         return sudokuBox;
-    }
-
-    private boolean isBoxCorrect() {
-        for (int row = 0; row < sudokuSize; row++) {
-            for (int column = 0; column < sudokuSize; column++) {
-                if (!this.getBox(row, column).verify()) {
-                    return false;
-                }
-            }
-        }
-        return true;
     }
 
     public int getValue(int row, int column) {
