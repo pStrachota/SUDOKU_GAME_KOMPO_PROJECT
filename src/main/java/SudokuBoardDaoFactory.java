a82a9e0bf35fb1e1f68fe3901b06fcdd417211ec
pStrachota <p.strachota52@gmail.com>
2022-02-13 17:07:26 +0100

    add sudokuBoardDaoFactory

---
+++ src/main/java/SudokuBoardDaoFactory.java
@@ -0,0 +1,9 @@
+public class SudokuBoardDaoFactory {
+
+    private SudokuBoardDaoFactory() {
+    }
+
+    public static Dao<SudokuBoard> getFileDao(String fileName) {
+        return new FileSudokuBoardDao(fileName);
+    }
+}
